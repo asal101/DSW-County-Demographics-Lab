b5c82344dc49a3393ed31e64f730732b32c82821
asal101 <77691090+asal101@users.noreply.github.com>
2021-03-16 09:45:13 -0700

    Update countyDemographicsUnfinished.py

--- countyDemographicsUnfinished.py
+++ countyDemographicsUnfinished.py
@@ -14,36 +14,50 @@ def main():
 def alphabetically_first_county(counties):
     """Return the county with the name that comes first alphabetically."""
     #Hint: you can use < to compare strings in Python. ex) "cat" < "dog" gives the value True
-    
+    first_county = counties[0]["County"]
+    for county in counties:
+        county["County"]
+        if county["County"] < first_county:
+            first_county = county["County"]
+    return first_county
     
 def percent_most_under_18(counties):
     """Return the highest percent of under 18 year olds."""    
+    first_county = counties[0]["Age"]["Percent Under 18 Years"]
+    for age in counties:
+        age["Age"]["Percent Under 18 Years"]
+        if age["Age"]["Percent Under 18 Years"] > first_county:
+            first_county = age["Age"]["Percent Under 18 Years"]
+    return first_county
     
-
-def county_most_under_18(counties):
     """Return the NAME of a county with the highest percent of under 18 year olds."""
-    
+def county_most_under_18(counties):
+    first_county = counties[0]["Age"]["Percent Under 18 Years"]
+    county_name = ""
+    for age in counties:
+        age["Age"]["Percent Under 18 Years"]
+        if age["Age"]["Percent Under 18 Years"] > first_county:
+            first_county = age["Age"]["Percent Under 18 Years"]
+            county_name = age["County"]
+    return county_name
     
 def lowest_median_income(counties):
     """Return a name of a county with the lowest median household income"""
-    
+    first_county = counties[0]["Income"]["Median Houseold Income"]
+    county_name = ""
+    for income in counties:
+        income["Income"]["Median Houseold Income"]
+        if income["Income"]["Median Houseold Income"] < first_county:
+            first_county = income["Income"]["Median Houseold Income"]
+            county_name = income["County"]
+    return county_name
+
     
 def high_income_counties(counties):
     """Return a LIST of the counties with a median household income over $90,000."""
-
-
-#To earn higher than a 3, complete one or both of the functions below
-def state_with_most_counties(counties):
-    """Return a state that has the most counties."""
-    #1. Make a dictionary that has a key for each state and the values keep track of the number of counties in each state
-    
-    #2. Find the state in the dictionary with the most counties
-    
-    #3. Return the state with the most counties
-    
-    
-def your_interesting_demographic_function(counties):
-    """Compute and return an interesting fact using the demographic data about the counties in the US."""
-
-if __name__ == '__main__':
-    main()
+    incomeList = []
+    for income in counties:
+        income["Income"]["Median Houseold Income"]
+        if income["Income"]["Median Houseold Income"] > 90000:
+            incomeList.append(income["County"])
+    return incomeList
